179761e91827b24c51e175aa37c4bdddd9fd7030
Ren3546 <128203284+Ren3546@users.noreply.github.com>
2024-02-08 23:06:01 -0500

    basic formatting, idletimeout function

---
+++ public/js/idleTimeout.js
@@ -0,0 +1,34 @@
+let timeoutId;
+
+function resetTimer() {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(logout, 300000); // 5 minutes in milliseconds
+}
+
+function logout() {
+  fetch('/api/user/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  })
+  .then(response => {
+    if (response.ok) {
+      alert("You have been logged out due to inactivity. Please log in again to make posts.");
+      window.location.replace('/');
+
+    } else {
+      alert('Failed to log out.');
+    }
+  })
+  .catch(error => {
+    console.error('Error', error);
+    alert('Failed to log out.');
+  });
+};
+
+
+document.addEventListener("mousemove", resetTimer);
+document.addEventListener("mousedown", resetTimer);
+document.addEventListener("keypress", resetTimer);
+document.addEventListener("touchstart", resetTimer);
+
+document.addEventListener("DOMContentLoaded", resetTimer);
